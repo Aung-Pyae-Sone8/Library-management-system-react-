099fdde69c89c6e0995f30cce9b61dbfccc39d26
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-04-13 19:09:34 +0630

    home page design

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+
+
+const HeroSection = () => {
+    return (
+        <div className="bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500 space-y-2 text-center h-48 flex flex-col justify-center items-center">
+            <h1 className="text-3xl">Welcome from my library</h1>
+            <p className="text-sm">A place where you can store and manage your books.</p>
+        </div>
+    )
+}
+
+export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
-
+import HeroSection from "../components/HeroSection"
+import book from "../assets/img1.png";
 
 function Home() {
 
   return (
-    <div className="text-blue-500">Home</div>
+    <>
+      {/* hero section */}
+      <HeroSection />
+
+      {/* book list  */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-3">
+        {[1, 2, 3, 4, 5].map((item) => (
+          <div className="p-4 border border-1" key={item}>
+            <img src={book} alt="book" />
+            <div className="text-center space-y-2 mt-3">
+              <h1>Book title</h1>
+              <p>Description</p>
+              {/* genres  */}
+              <div className="flex flex-wrap">
+                {
+                  ['travel','knowledge','fiction','science'].map((genre) => (
+                    <span key={genre} className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500">{genre}</span>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
