8a6ca4a4e49a3f2ec0a79fbe80d8a2694b870710
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-04-18 17:40:15 +0630

    book create form design

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,61 @@
+import { useState } from "react";
 
 
 const Create = () => {
+
+    let [title, setTitle] = useState('');
+    let [description, setDescription] = useState('');
+    let [newCategory, setNewCategory] = useState('');
+
     return (
-        <div>
-            create page
-        </div>
+        <form className="w-full max-w-lg mx-auto mt-5">
+
+            {/* book title          */}
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Book Title
+                    </label>
+                    <input value={title} onChange={e => setTitle(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Book Title" />
+                </div>
+            </div>
+
+            {/* book description  */}
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Book Description
+                    </label>
+                    <textarea value={description} onChange={e => setDescription(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Book Description" />
+                </div>
+            </div>
+
+            {/* book categories  */}
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                        Categories
+                    </label>
+                    <div className="flex items-center space-x-2">
+                        <input value={newCategory} onChange={e => setNewCategory(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Book Category" />
+                        <button className="bg-primary p-1 rounded-lg text-white mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            {/* create book  */}
+            <button className='text-white bg-primary px-3 py-2 rounded-2xl flex items-center gap-1 w-full justify-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+
+                <span className='hidden md:block'>Create book</span>
+            </button>
+        </form>
     )
 }
 
