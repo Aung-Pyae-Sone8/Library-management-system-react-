f66d830a4a0a217054413834a5f7ed67c7f7e3c8
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-04-18 17:17:45 +0630

    book detail page design

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch';
+import bookImg from '../assets/img1.png'
 
 const BookDetail = () => {
 
@@ -11,7 +12,22 @@ const BookDetail = () => {
             {error && <p>{error}</p>}
             {loading && <p>loading...</p>}
             {book && (
-                <h1>{book.title}</h1>
+                <div className='grid grid-cols-2'>
+                    <div>
+                        <img src={bookImg} alt={book.title} className='w-[80%]' />
+                    </div>
+                    <div className='space-y-4'>
+                        <h1 className='text-3xl font-bold'>{book.title}</h1>
+                        <div className='space-x-3'>
+                            {book.categories.map((category) => (
+                                <span className='bg-blue-500 text-white rounded-full text-sm px-2 py-1' key={category}>{category}</span>
+                            ))}
+                        </div>
+                        <p>
+                            {book.description}
+                        </p>
+                    </div>
+                </div>
             )}
         </>
     )
